654801fed0a0f7a067c04199fc2980d08c742bdd
Intern097 <john.theocharides@gmail.com>
2020-11-20 14:35:24 +0200

    Changed my sample data for y to use tf.keras.utils.to_categorical and changed number of classes from 4 to 10.

--- Complex_custom_loss.py
+++ Complex_custom_loss.py
@@ -13,27 +13,24 @@ import random
 import math
 
 # x and y are defined as our sample data
-x = np.asarray(tf.random.uniform(minval=0, maxval=1, shape=(6400, 4), dtype=tf.float32))
-y = np.asarray(tf.multiply(tf.reduce_sum(x, axis=-1), 7))
+x = np.asarray(tf.random.uniform(minval=0, maxval=1, shape=(6400, 10), dtype=tf.float32))
+y = keras.utils.to_categorical(np.random.randint(10, size=(6400, 1)), num_classes=10)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-x_train = x_train.reshape((-1, 4, 1, 1))
-x_test = x_test.reshape((-1, 4, 1, 1))
-y_train = y_train.reshape((-1, 1))
-y_test = y_test.reshape((-1, 1))
-
+x_train = x_train.reshape((-1, 10, 1, 1))
+x_test = x_test.reshape((-1, 10, 1, 1))
 
 # Hyperparameters
 weight_init = RandomNormal()
 opt = Adam(lr=0.001)
 batch_size = 128
-epochs = 50
+epochs = 15
 # [n:(n+batch_size)]
 
 # Builds model that we will use for the training process
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(1, 1), activation='relu', kernel_initializer=weight_init, input_shape=(4, 1, 1)))
+model.add(Conv2D(32, kernel_size=(1, 1), activation='relu', kernel_initializer=weight_init, input_shape=(10, 1, 1)))
 model.add(Conv2D(64, (1, 1), activation='relu', kernel_initializer=weight_init))
 model.add(MaxPooling2D(pool_size=(1, 1)))
 model.add(Dropout(0.25))
@@ -43,7 +40,7 @@ hidden_layer_1 = Dense(128, activation='relu', kernel_initializer=weight_init)
 model.add(hidden_layer_1)
 hidden_layer_2 = Dropout(0.3)
 model.add(hidden_layer_2)
-output_layer = Dense(1, activation='softmax', kernel_initializer=weight_init)
+output_layer = Dense(10, activation='softmax', kernel_initializer=weight_init)
 model.add(output_layer)
 model.summary()
 # keras.utils.plot_model(model, show_shapes=True)
@@ -63,7 +60,7 @@ model.summary()
 def step(real_x, real_y):
     with tf.GradientTape() as tape:
         # Make prediction
-        pred_y = model(real_x.reshape((-1, 4, 1, 1)))
+        pred_y = model(real_x.reshape((-1, 10, 1, 1)))
         # Calculate loss
         model_loss = categorical_crossentropy(real_y, pred_y)
 
